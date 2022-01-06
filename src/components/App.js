2da4aa0fd8fb390265cfe67f39a6d9754dc70630
Adam <adamlenter1@gmail.com>
2022-01-06 14:27:37 -0500

    commit tree 45c48ac6bcfb96766eb38521c5eb04a9a8e8a2ca

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,10 +6,51 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+  const [questionList, setQuestionList] = useState([])
+  const [questionsLoaded, setQuestionsLoaded] = useState(false)
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions")
+      .then((r)=>r.json())
+      .then((questionList) => setQuestionList(questionList))
+      .then(()=>setQuestionsLoaded(true))   
+      }, [])
+
+  function addQuestion(formData) {
+    const newQuestionList = [...questionList, formData];
+    setQuestionList(newQuestionList)
+    setPage("List")
+  }
+
+  function deleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"}
+      )
+
+    const newQuestionList = questionList.filter((question) => question.id !== id);
+    setQuestionList(newQuestionList);
+  }
+
+  function updateCorrectAnswer(id, updatedQuestion) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+        },
+      body: JSON.stringify(
+          {
+          correctIndex: updatedQuestion.correctIndex
+          }
+        )
+      }
+    )
+    console.log(id);
+  }
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestion = {addQuestion} /> : <QuestionList questionList = {questionList} questionsLoaded = {questionsLoaded} deleteQuestion = {deleteQuestion} updateCorrectAnswer = {updateCorrectAnswer} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,29 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
-  );
+function QuestionList( {questionList, questionsLoaded, deleteQuestion, updateCorrectAnswer } ) {  
+  if(questionsLoaded) {
+    return (
+      <section>
+        <h1>Quiz Questions</h1>
+        <ul>
+          {questionList.map((question) => {
+            
+            console.log(question.id)
+
+            return(
+              <QuestionItem key={question.id} question={question} deleteQuestion = {deleteQuestion} updateCorrectAnswer = {updateCorrectAnswer} />
+            )
+          })}
+        </ul>
+      </section>
+    );
+  }
+  else {
+    return (
+      <p>Loading...</p>
+    )
+  }
 }
 
 export default QuestionList;
